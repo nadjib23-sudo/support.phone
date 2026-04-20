c01ea9e32f1337bb1deead7b636f8133a9f37f22
nadjib23-sudo <nadjibdaoudi2005@gmail.com>
2026-04-20 12:49:27 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -171,8 +171,8 @@ export default function App() {
             >
               <div className="relative z-10 rounded-[2.5rem] overflow-hidden shadow-2xl shadow-slate-200 border-8 border-white">
                 <img 
-                  src="https://qmax.qa/ar/products/vacuum-suction-magnetic-phone-holder-mobile-mount-k007?srsltid=AfmBOopFl1-shtVIFTanHWNy46KFbTcNPG8iri1aR2jqjsQqepo0HOkF" 
-                  alt="Magnetic Car Phone Holder" 
+                  src="product.jpg" 
+                  alt="product" 
                   className="w-full aspect-square object-cover"
                   referrerPolicy="no-referrer"
                 />
